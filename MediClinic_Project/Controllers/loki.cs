90c3241fa5d12cb554186b8c07f5fd67a89dff6a
DotNetDynamics <bashmiranjithkumar80@gmail.com>
2026-04-08 15:37:22 +0530

    added controller

---
+++ MediClinic_Project/Controllers/loki.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MediClinic_Project.Controllers
+{
+    public class loki : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
